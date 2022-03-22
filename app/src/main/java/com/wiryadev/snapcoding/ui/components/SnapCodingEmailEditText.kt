305aa30233e082aa1505fe12167f09a98284c9b5
wiryadev <abrarwiryawan@gmail.com>
2022-03-22 08:56:13 +0700

    update PasswordEditText and add EmailEditText

---
+++ app/src/main/java/com/wiryadev/snapcoding/ui/components/SnapCodingEmailEditText.kt
@@ -0,0 +1,71 @@
+package com.wiryadev.snapcoding.ui.components
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.TextUtils
+import android.util.AttributeSet
+import android.util.Patterns
+import android.widget.EditText
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.wiryadev.snapcoding.R
+
+class SnapCodingEmailEditText : TextInputEditText {
+
+    private var errorDrawable: Drawable? = null
+    private var parentInputLayout: TextInputLayout? = null
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        parentInputLayout = findViewById<EditText>(id).parent.parent as TextInputLayout
+    }
+
+    private fun init() {
+        errorDrawable = ContextCompat.getDrawable(context, R.drawable.ic_exclamation)
+        addTextChangedListener(
+            onTextChanged = { text, _, _, _ ->
+                if (!isValidEmail(text.toString())) {
+                    showError()
+                }
+            },
+            afterTextChanged = {
+                if (isValidEmail(it.toString())) {
+                    hideError()
+                }
+            },
+        )
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    private fun showError() {
+        parentInputLayout?.apply {
+            error = resources.getString(R.string.error_email_invalid)
+            errorIconDrawable = errorDrawable
+        }
+    }
+
+    private fun hideError() {
+        parentInputLayout?.apply {
+            error = null
+            errorIconDrawable = null
+        }
+    }
+}
---
+++ app/src/main/java/com/wiryadev/snapcoding/ui/components/SnapCodingPasswordEditText.kt
@@ -0,0 +1,69 @@
+package com.wiryadev.snapcoding.ui.components
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.method.PasswordTransformationMethod
+import android.util.AttributeSet
+import android.widget.EditText
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.wiryadev.snapcoding.R
+
+class SnapCodingPasswordEditText : TextInputEditText {
+
+    private var errorDrawable: Drawable? = null
+    private var parentInputLayout: TextInputLayout? = null
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        parentInputLayout = findViewById<EditText>(id).parent.parent as TextInputLayout
+    }
+
+    private fun init() {
+        errorDrawable = ContextCompat.getDrawable(context, R.drawable.ic_exclamation)
+
+        transformationMethod = PasswordTransformationMethod.getInstance()
+
+        addTextChangedListener(
+            onTextChanged = { _, _, _, count ->
+                if (count < 6) {
+                    showError()
+                }
+            },
+            afterTextChanged = {
+                if (it.toString().length > 6) {
+                    hideError()
+                }
+            },
+        )
+    }
+
+    private fun showError() {
+        parentInputLayout?.apply {
+            error = resources.getString(R.string.error_field_length)
+            errorIconDrawable = errorDrawable
+        }
+    }
+
+    private fun hideError() {
+        parentInputLayout?.apply {
+            error = null
+            errorIconDrawable = null
+        }
+    }
+}
