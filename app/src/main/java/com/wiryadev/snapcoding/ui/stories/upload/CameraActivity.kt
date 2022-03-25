7adf08d48397909c81179f77e488326c2fea8c08
wiryadev <abrarwiryawan@gmail.com>
2022-03-25 17:59:34 +0700

    add layout for upload and camera screen

---
+++ app/src/main/java/com/wiryadev/snapcoding/ui/stories/upload/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.wiryadev.snapcoding.ui.stories.upload
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wiryadev.snapcoding.R
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
